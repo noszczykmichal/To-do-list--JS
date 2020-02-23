f374c50e044a9fea4ff8da22886f20ae1a545332
Michał Noszczyk <noszczykmichal@wp.pl>
2020-02-23 14:04:18 +0100

    dodanie mozliwosci odchaczenia pozycji na liscie, usuniecie elementu + przechowywanie danych w schowku

--- js/main.js
+++ js/main.js
@@ -11,8 +11,34 @@ const uncheck="fa-circle";
 const lineThrough="lineThrough";
 
 //zmienne
-let objList=[];
-let id=0;
+let objList, id;
+
+//pobieranie elementów ze schowka
+let data=localStorage.getItem("TODO");
+
+//sprawdzanie czy w schowków przetrzymywane są dane
+
+if(data){
+    objList=JSON.parse(data);
+    id=objList.length; //określamy jakie id będzie mieć kolejny dodany element sprawdzając długość tablicy
+    LoadObjList(objList); //załadowanie listy po odświeżeniu strony na podstawie danych pobranych ze schowka
+}else{ //jeśli schowek jest pusty- ustawiamy zmienną objList jako tablicę, a wartość id na równą 0
+    objList=[];
+    id=0;
+}
+
+//dodanie funkcji LoadObjList, która załaduje listę elementów przechowywanych w schowku
+function LoadObjList(array){
+    array.forEach(function(item){
+        addToDo(item.name, item.id, item.done, item.trash)
+    });
+}
+
+//usuwanie danych ze schowka
+
+clear.addEventListener("click", function(){
+    localStorage.clear
+})
 
 //wyświetlanie aktualnej daty
 const options= {weekday:"short", month:"long", day:"numeric"}
@@ -57,14 +83,53 @@ document.addEventListener('keyup', function(event){
 
             });
 
+            //dodawanie elementów do schowka (kod dodawany w każdym miejscu gdzie dopisywane są dane do tablicy z elementami listy)
+            localStorage.setItem("TODO", JSON.stringify(objList));
+
             id++;
         }
         input.value="";
     }
     
-})
+});
 
-addToDo("Kawa", 1, false, true);
+//funkcja completeToDo->zmiana klas po zaznaczeniu "tick'a" przy pozycji na liście
+
+function completeToDo(element){
+    element.classList.toggle(check);
+    element.classList.toggle(uncheck);
+    element.parentNode.querySelector(".text").classList.toggle(lineThrough);
+
+    if(objList[element.id].done==true){
+        return objList[element.id].done==false;
+    }else{
+        return objList[element.id].done==true;
+    }
+}
+
+//funkcja removeToDo-> usunięcie elemntu z listy użytkownika
+
+function removeToDo(element){
+    element.parentNode.parentNode.removeChild(element.parentNode);
+    objList[element.id].trash=true;
+}
+
+//dodanie addEventListener'a do dynamicznie tworzonych elementów listy
+
+list.addEventListener("click", function(event){
+    const element=event.target; //zwraca kliknięty element wewnatrz listy
+    const elementJob=element.attributes.job.value;//pobranie atrybutu job elementu: complete || delete
+
+    if(elementJob =="complete"){
+        completeToDo(element)
+    }else if(elementJob=="delete"){
+        removeToDo(element);
+    }
+
+    //dodawanie elementów do schowka (kod dodawany w każdym miejscu gdzie dopisywane są dane do tablicy z elementami listy)
+    localStorage.setItem("TODO", JSON.stringify(objList));
+
+});
 
 
 
